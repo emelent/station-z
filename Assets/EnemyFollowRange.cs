53f85dc314592b3379d2219fc1c2e07f3343ae73
Roy <technicmk@gmail.com>
2018-04-05 22:16:20 +0200

    Add zombie target sharing

---
+++ Assets/EnemyFollowRange.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFollowRange : MonoBehaviour {
+	public SightRange sightRange;
+	void OnTriggerEnter2D(Collider2D collider){
+		if(collider.tag == "Enemy"){
+			print("entered");
+			SightRange s = collider.GetComponent<ZombieAI>().vision;
+			if(s.target == null)
+				s.target = sightRange.target;
+
+		}
+	}
+}
